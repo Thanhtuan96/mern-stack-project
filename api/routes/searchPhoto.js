1014df39815a98d23cf00f8154325d8e1d40daee
Ronnie.fs.315 <42103638+Thanhtuan96@users.noreply.github.com>
2021-05-10 21:24:41 +0900

    still working on with backend

--- api/routes/searchPhoto.js
+++ api/routes/searchPhoto.js
@@ -2,10 +2,14 @@ var express = require('express');
 var router = express.Router();
 const bodyParser = require('body-parser');
 const querystring = require('querystring');
+var axios = require('axios');
+var Unsplash = require('unsplash-js');
 
-router.get('/', (req, res, next) => {
-  const searchquerry = req.query.searchInput;
-  res.json(searchquerry);
-});
+// const unsplash = new Unsplash({
+//   applicationId: 'AMda7xjxKmvDRZKjtGXyl8bVcOUnP98Tt-Dr5aJ8o8k',
+//   secret: 'ZY7oUS6ogCY3SO3BuNm8E-ACFk0dOzdDHzmyC3WIRS8',
+// });
+
+router.post('/', (req, res, next) => {});
 
 module.exports = router;
